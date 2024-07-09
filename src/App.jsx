b87f6c42ae499ab4b6382bb99775cce9b6847af4
Mirodiluser <hayitovmirodil12@gamil.com>
2024-07-09 09:43:25 +0500

    asadas

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom' 
+
+export async function loader() {
+  const contacts = await getContacts();
+  return {contacts};
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      6
+      <Navbar />
+      <Outlet />
     </>
   )
 }
